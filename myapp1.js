606c61b7ef7648ffe8993861e2675c0161187bf4
Akshay0622 <akshaytanpure226@gmail.com>
2024-04-01 00:01:24 +0530

    myjavascript new app

---
+++ myapp1.js
@@ -0,0 +1,8 @@
+// JavaScript is a versatile programming language primarily used for creating interactive web content.
+// It was initially developed by Netscape as a client-side scripting language but has since expanded 
+// its use to server-side development (Node.js), mobile app development (React Native, Ionic), 
+// game development (Unity, Phaser), and more.
+
+
+
+console.log('Hello World');
