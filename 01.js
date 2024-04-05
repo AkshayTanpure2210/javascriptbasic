8f64db74037e6de43e07239c0f776bd0742fe961
Akshay0622 <akshaytanpure226@gmail.com>
2024-04-06 00:43:52 +0530

    error solve

---
+++ 01.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//notes which i observe
+
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+var firstName = "Akshay";
+
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "Rushi";
+
+console.log(firstName);
--- 02.js
+++ 02.js
@@ -1,21 +1,24 @@
-"use strict";
+// rules for naming variables 
 
-//notes which i observe
+// you cannot start with number 
+// example :- 
+// 1value (invalid)
+// value1 (valid)
 
+var value1 = 2;
+console.log(value1);
 
-// variables can store some information
-// we can use that information later
-// we can change that information later
+// you can use only undersore _ or dollar symbol 
+// first_name (valid)
+// _firstname (valid) 
 
-// declare a variable 
-var firstName = "Akshay";
+// first$name (valid)
+// $firstname (valid)
 
+// you cannot use spaces 
+// var first_name = "harshit"; // snake case writing 
+// var firstName = "harshit"; // camel case writing 
+// first name (invalid)
 
-// use a variable 
-console.log(firstName);
-
-// change value 
-
-firstName = "Rushi";
-
-console.log(firstName);
+// convention 
+// start with small letter and use camelCase 
